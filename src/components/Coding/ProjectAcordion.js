76774513b894d6754475300c47c40b5daeab0ad0
Irene <irenemanningm@gmail.com>
2024-02-07 06:24:31 -0600

    Add A function to take in a url and open in a new tab and apply to all my project demos and github links.

--- src/components/Coding/ProjectAcordion.js
+++ src/components/Coding/ProjectAcordion.js
@@ -2,6 +2,10 @@ import React from "react"
 import { Accordion, Button } from "react-bootstrap"
 
 function ProjectAcordion() {
+
+    const openInNewTab = (url) => {
+        window.open(url, '_blank', 'noreferrer');
+    }
     return (
         <div className="project-accordion">
             <Accordion flush  className="w-100">
@@ -12,8 +16,8 @@ function ProjectAcordion() {
                         <h6>
                         A text-centric social media platform where users share the latest gossip through text-only posts.
                         </h6>
-                        <Button variant="success" href="https://gossipgrl.onrender.com/">DEMO</Button>
-                        <Button variant="outline-success">GitHub</Button>
+                        <Button variant="success" onClick={() => openInNewTab("https://gossipgrl.onrender.com/")}>DEMO</Button>
+                        <Button variant="outline-success" onClick={() => openInNewTab("https://github.com/irenemanning/gossip_grl")}>GitHub</Button>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
@@ -22,8 +26,8 @@ function ProjectAcordion() {
                         <h6>
                         A collaborative Ruby on Rails application for sharing and reviewing diverse wines, allowing users to contribute by adding new entries and leaving reviews.
                         </h6>
-                        <Button variant="success" href="https://wine-reviewer.onrender.com/">DEMO</Button>
-                        <Button variant="outline-success">GitHub</Button>
+                        <Button variant="success" onClick={() => openInNewTab("https://wine-reviewer.onrender.com/")}>DEMO</Button>
+                        <Button variant="outline-success" onClick={() => openInNewTab("https://github.com/irenemanning/wine-reviewer")}>GitHub</Button>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
@@ -32,8 +36,8 @@ function ProjectAcordion() {
                         <h6>
                         A whimsical web app enabling users to create, view, edit, delete, and like cat memes, with dynamic content management via a JSON database.
                         </h6>
-                        <Button variant="success" href="https://cat-memes-by-irene.netlify.app/">DEMO</Button>
-                        <Button variant="outline-success">GitHub</Button>
+                        <Button variant="success" onClick={() => openInNewTab("https://cat-memes-by-irene.netlify.app/")}>DEMO</Button>
+                        <Button variant="outline-success" onClick={() => openInNewTab("https://github.com/irenemanning/cat-meme-generator")} >GitHub</Button>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
@@ -42,8 +46,8 @@ function ProjectAcordion() {
                         <h6>
                         A JavaScript web app using a Pokemon API for browsing and collecting, with enhanced user experience through event listeners and animations.
                         </h6>
-                    <Button variant="success" href="https://poke-liker.netlify.app/">DEMO</Button>
-                    <Button variant="outline-success">GitHub</Button>
+                    <Button variant="success" onClick={() => openInNewTab("https://poke-liker.netlify.app/")}>DEMO</Button>
+                    <Button variant="outline-success" onClick={() => openInNewTab("https://github.com/irenemanning/poke-project")}>GitHub</Button>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
